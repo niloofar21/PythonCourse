69d4b0177546af2d1b7b5566604a1d5c65731617
niloofar21 <92670985+niloofar21@users.noreply.github.com>
2021-10-31 02:34:03 +0300

    Session3-05/10/2021

--- inclass activities/4_lab1.py
+++ inclass activities/4_lab1.py
@@ -15,3 +15,28 @@ def int_to_base(num, base):
         digits.insert(0,num%base)
         num=num//base
     return ''.join(str(e) for e in digits)
+
+'''
+"""take a string-formatted number and its base and return the base-10 integer"""
+def base_to_int(string, base):
+    if string=="0" or base <= 0 : return 0
+    result = 0
+    return result
+
+
+
+def flexibase_add(str1, str2, base1, base2):
+  """add two numbers of different bases and return the sum"""
+  result = int_to_base(tmp, base1)
+  return result
+
+def flexibase_multiply(str1, str2, base1, base2):
+  """multiply two numbers of different bases and return the product"""
+  result = int_to_base(tmp, base1)
+  return result
+
+def romanify(num):
+  """given an integer, return the Roman numeral version"""
+  result = ""
+  return result
+'''
