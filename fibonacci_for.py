81e50e3c5c454d27216f830555c84d8d6c9bcd5a
niloofar21 <92670985+niloofar21@users.noreply.github.com>
2021-10-30 21:22:08 +0300

    Add files via upload

---
+++ fibonacci_for.py
@@ -0,0 +1,12 @@
+Fib = [1,1]
+for i in range(2,10):
+    Fib.append(Fib[i-2]+Fib[i-1])
+
+
+''' "another way to do the same thing"
+
+Fib = [1,1]
+for i in range(1,9):
+    Fib.append(Fib[-2]+Fib[-1])
+
+"This will give the same output" '''
